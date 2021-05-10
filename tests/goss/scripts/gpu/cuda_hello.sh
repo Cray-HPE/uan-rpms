4e46462148745db8050ffa3b786c0bec05f30d00
Alan Mutschelknaus <alanm@cray.com>
2021-05-09 21:06:49 -0500

    Fix cudatoolkit module load

--- tests/goss/scripts/gpu/cuda_hello.sh
+++ tests/goss/scripts/gpu/cuda_hello.sh
@@ -6,8 +6,22 @@ WORKDIR=$(mktemp -d)
 cp $ROOTDIR/* $WORKDIR
 pushd $WORKDIR
 
-module load cudatoolkit
+# module unfortunately does not have correct error codes
+# pipe stderr to stdout and parse the output to determine
+# failure
+module load cudatoolkit 2>&1 | grep "Unable to locate"
+if [ $? -eq 0 ]; then
+  echo Could not load cudatoolkit
+  echo FAIL
+  exit 1
+fi
+
 make
+if [ $? -ne 0 ]; then
+  echo make command failed
+  echo FAIL
+  exit 1
+fi
 
 ./hello
 
